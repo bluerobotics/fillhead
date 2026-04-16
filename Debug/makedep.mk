e396e64dd30e94ffc8bfcd486a96b79e30a8e901
Zachariah Mears <zx100x100@gmail.com>
2026-04-16 12:12:17 -0700

    Move arduino sketch to subdirectory as required and fix gitignore

---
+++ Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Device_Startup\startup_same53.c
+
+src\fillhead.cpp
+
+src\heater_controller.cpp
+
+src\comms_controller.cpp
+
+src\injector_controller.cpp
+
+src\pinch_valve_controller.cpp
+
+src\vacuum_controller.cpp
+
+src\error_log.cpp
+
+src\commands.cpp
+
+src\events.cpp
+
+src\force_sensor.cpp
+
